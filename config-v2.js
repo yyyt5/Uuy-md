96d00d4161fa4921dc3b97c4b69cd713e0457ae5
yyyt5 <yrandula0@gmail.com>
2025-05-18 19:42:22 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~dnVXQI6Q#NIm8vnuoUooFc68SDDU4YF3P0RkcSDoZANQTzA2rOhk"
 
 
 module.exports = {
